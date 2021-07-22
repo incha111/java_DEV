39323b837531fb8808e56c4d2373f9e3ad24ca72
incha111 <87800456+incha111@users.noreply.github.com>
2021-07-22 20:37:16 +0200

    Tuto1 - Using variables

--- src/Application.java
+++ src/Application.java
@@ -2,9 +2,66 @@
 public class Application {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("Hello World!");
+		// System.out.println("Hello World!");
+		String str = "Hello World!";
+		displayVariable(str);
+		
+		byte myByte = 102;
+		short myShort = 16;
+		int myInt = 32500;
+		long myLong = 124578;
+		
+		float myFloat = 32.5f;
+		double myDouble = 2514.235;
+		
+		char myChar = 'C';
+		boolean myBoolean = true;
+		
+		displayVariable(myByte);
+		displayVariable(myShort);
+		displayVariable(myInt);
+		displayVariable(myLong);
+		displayVariable(myFloat);
+		displayVariable(myDouble);
+		displayVariable(myChar);
+		displayVariable(myBoolean);
 
+	}
+	
+	public static void displayVariable(byte myByte) {
+		System.out.println(myByte);
+	}
+	
+	public static void displayVariable(short myShort) {
+		System.out.println(myShort);
+	}
+	
+	public static void displayVariable(int myInt) {
+		System.out.println(myInt);
+	}
+	
+	public static void displayVariable(long myLong) {
+		System.out.println(myLong);
+	}
+	
+	public static void displayVariable(float myFloat) {
+		System.out.println(myFloat);
+	}
+	
+	public static void displayVariable(double myDouble) {
+		System.out.println(myDouble);
+	}
+	
+	public static void displayVariable(char myChar) {
+		System.out.println(myChar);
+	}
+	
+	public static void displayVariable(boolean myBoolean) {
+		System.out.println(myBoolean);
+	}
+	
+	public static void displayVariable(String str) {
+		System.out.println(str);
 	}
 
 }
