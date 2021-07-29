4ba001d67e8b5ca80f4fe23da9a08df2b4dd575a
incha111 <87800456+incha111@users.noreply.github.com>
2021-07-29 10:03:41 +0200

    Tuto3 - Adding a switch

--- Tuto3 - Scanner - Arrays/src/Application.java
+++ Tuto3 - Scanner - Arrays/src/Application.java
@@ -3,10 +3,33 @@ import java.util.Scanner;
 public class Application {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		
-		System.out.println("Enter a sentence please: "); // Creating a scanner object.
-		String input = scanner.nextLine(); // nextLine() is for Strings; nextInt() is for integers; nextDouble() is for doubles.
-		
-		System.out.printf("You entered: %s",input.toString());
+
+		/*
+		 * System.out.println("Enter a sentence please: "); // Creating a scanner
+		 * object. String input = scanner.nextLine(); // nextLine() is for Strings;
+		 * nextInt() is for integers; nextDouble() is for doubles.
+		 * 
+		 * System.out.printf("You entered: %s",input.toString());
+		 */
+
+		System.out.println("Enter a number between 1 and 4 please: ");
+		int input = scanner.nextInt();
+
+		switch (input) {
+		case 1:
+			System.out.println("You chose number 1!");
+			break;
+		case 2:
+			System.out.println("You chose number 2!");
+			break;
+		case 3:
+			System.out.println("You chose number 3!");
+			break;
+		case 4:
+			System.out.println("You chose number 4!");
+			break;
+		default:
+			System.out.println("Your choice is invalid.");
+		}
 	}
 }
