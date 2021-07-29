80d569f48ef72187fda4b451892153ff8516e735
incha111 <87800456+incha111@users.noreply.github.com>
2021-07-29 09:36:13 +0200

    Tuto2 - Using "printf"

--- Tuto2 - Conditions/src/Application.java
+++ Tuto2 - Conditions/src/Application.java
@@ -2,12 +2,18 @@
 public class Application {
 	public static void main(String[] args) {
 		int myVariable = 5;
+		String str = "Hello World!";
 		
 		for(int i = 0; i < 10; i++) {
 			if(i == myVariable) {
 				continue;
 			}
-			System.out.printf("Value of i =%d\n", i);
+			
+			System.out.printf("Value of i = %d\n", i);
+			
+			if(i == 7) {
+				System.out.printf(" - %s\n",str);
+			}
 		}
 	}
 }
