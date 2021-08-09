d2ec04c44a59bf5a232412723a6bf6c1deb44595
incha111 <87800456+incha111@users.noreply.github.com>
2021-08-09 22:38:09 +0200

    Tuto4 - Data - Methods

--- Tuto4 - Classes/src/Application.java
+++ Tuto4 - Classes/src/Application.java
@@ -1,17 +1,19 @@
 
 public class Application {
 	public static void main(String[] args) {
-		Person person1 = new Person();
-		person1.firstName = "Incha";
+		Person person1 = new Person("Incha","N'Goy",47);
+		/*person1.firstName = "Incha";
 		person1.lastName = "N'Goy";
-		person1.age = 47;
+		person1.age = 47;*/
 		
-		Person person2 = new Person();
-		person2.firstName = "Lisa";
+		Person person2 = new Person("Lisa","Matton",38);
+		/*person2.firstName = "Lisa";
 		person2.lastName = "Matton";
-		person2.age = 38;
+		person2.age = 38;*/
 		
 		person1.showInfo();
+		person1.sayHello();
 		person2.showInfo();
+		person2.speak("Hello there. I'm " + person2.firstName);
 	}
 }
--- Tuto4 - Classes/src/Person.java
+++ Tuto4 - Classes/src/Person.java
@@ -5,9 +5,24 @@ public class Person {
 	String lastName;
 	int age;
 	
+	// Constructor
+	public Person(String firstName, String lastName, int age) {
+		this.firstName = firstName;
+		this.lastName = lastName;
+		this.age = age;
+	}
+	
 	// Method
 	public void showInfo() {
 		//System.out.println("My name is " + firstName + " " + lastName + " and I am " + age + " years old.");
 		System.out.printf("My name is %s %s and I'm %d years old.\n",firstName,lastName,age);
+	}
+	
+	public void sayHello() {
+		System.out.println("Hello!");
+	}
+	
+	public void speak (String str) {
+		System.out.println(str);
 	}
 }
